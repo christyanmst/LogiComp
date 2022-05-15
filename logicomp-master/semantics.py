6f01140ccf7e7d6e02c0e1e5c9aaf8e17cfeb500
christyanmst <christyan228@gmail.com>
2022-05-15 11:42:51 -0300

    Create Truth_value 1/2
    Definindo a valoração das minhas atômicas

--- logicomp-master/semantics.py
+++ logicomp-master/semantics.py
@@ -1,17 +1,15 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
 
-
 from formula import *
 from functions import atoms
 
-
-def truth_value(formula, interpretation):
-    """Determines the truth value of a formula in an interpretation (valuation).
-    An interpretation may be defined as dictionary. For example, {'p': True, 'q': False}.
-    """
-    pass
-    # ======== YOUR CODE HERE ========
-
+def truth_value(formula):
+    dicionario ={}
+    for x in atoms(formula):
+        print("Atômica: ",x)
+        interpretation = int(input('Digite uma valoração para essa atômica, 0 ou 1:\n'))
+        dicionario[x]= bool(interpretation)
+    return dicionario
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
     """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
