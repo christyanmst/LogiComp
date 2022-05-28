2c23d81b6ffb27b78059c9259817a9c226b52ef7
christyanmst <christyan228@gmail.com>
2022-05-27 22:50:33 -0300

    Atualizando Truth value
    Até o momento, funcionando apenas para os casos que não tem implicação

--- logicomp-master/semantics.py
+++ logicomp-master/semantics.py
@@ -3,13 +3,23 @@
 from formula import *
 from functions import atoms
 
-def truth_value(formula):
+def aux_truth_value(formula):
     dicionario ={}
     for x in atoms(formula):
         print("Atômica: ",x)
         interpretation = int(input('Digite uma valoração para essa atômica, 0 ou 1:\n'))
         dicionario[x]= bool(interpretation)
     return dicionario
+    
+def truth_value(formula):
+    dicionario = aux_truth_value(formula)
+    valores =[]
+    valores.append(dicionario)
+    for i in valores:
+        for k, v in i.items():
+            formula = str(formula).replace(str(k),str(v)).replace('∧', 'and').replace('∨','or').replace('¬','not ')
+            #print(k +": "+str(v))
+    return(eval(formula))
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
     """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
