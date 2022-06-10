64f6512ecfd110f0f0b4ba925f2a2c544703e160
christyanmst <christyan228@gmail.com>
2022-06-10 10:43:06 -0300

    Implementando Truth_table, Satisfability_brute_force e Is_valid

--- logicomp-master/semantics.py
+++ logicomp-master/semantics.py
@@ -1,25 +1,37 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
-
+import ttg
+from ctypes import Union
 from formula import *
 from functions import atoms
 
 def aux_truth_value(formula):
     dicionario ={}
     for x in atoms(formula):
-        print("Atômica: ",x)
-        interpretation = int(input('Digite uma valoração para essa atômica, 0 ou 1:\n'))
-        dicionario[x]= bool(interpretation)
+        print("\nATÔMICA: ",x)
+        interpretation = eval(input('Digite uma valoração para essa atômica, True ou False:\n'))
+        dicionario[x]= interpretation
+    print(dicionario)
     return dicionario
     
 def truth_value(formula):
     dicionario = aux_truth_value(formula)
-    valores =[]
-    valores.append(dicionario)
-    for i in valores:
-        for k, v in i.items():
-            formula = str(formula).replace(str(k),str(v)).replace('∧', 'and').replace('∨','or').replace('¬','not ')
-            #print(k +": "+str(v))
-    return(eval(formula))
+    print(formula)
+    for atom, value in dicionario.items():
+        formula = str(formula).replace(str(atom), str(value))
+    formula = str(formula).replace('∧', ' and ').replace('∨',' or ').replace('¬',' not ')
+    print(formula)
+    print(eval(formula))
+
+def satisfiability_brute_force(formula):
+    table=truth_table(formula)
+    print(table)
+    if table.valuation() == 'Contingency':
+        print("A Fórmula é satisfatível")
+    if table.valuation() == 'Tautology':
+        print("A Fórmula é Válida")
+    if table.valuation() == 'Contradiction':
+        print("A Fórmula é insatisfatível")
+
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
     """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
@@ -34,16 +46,15 @@ def is_logical_equivalence(formula1, formula2):
 
 
 def is_valid(formula):
-    """Returns True if formula is a logically valid (tautology). Otherwise, it returns False"""
-    pass
+    table=truth_table(formula)
+    if table.valuation() == 'Tautology':
+        return True 
+    else:
+        return False
     # ======== YOUR CODE HERE ========
 
-
-def satisfiability_brute_force(formula):
-    """Checks whether formula is satisfiable.
-    In other words, if the input formula is satisfiable, it returns an interpretation that assigns true to the formula.
-    Otherwise, it returns False."""
-    pass
-    # ======== YOUR CODE HERE ========
-
+def truth_table(formula):
+    strFormula = str(formula).replace('∧', ' and ').replace('∨',' or ').replace('¬',' not ')
+    table = ttg.Truths(list(atoms(formula)),[strFormula])
+    return table
 
