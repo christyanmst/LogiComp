e20ac58d2b51c670888a36591c90e25a7cb529dd
christyanmst <christyan228@gmail.com>
2022-06-10 20:04:55 -0300

    Update semantics

--- logicomp-master/semantics.py
+++ logicomp-master/semantics.py
@@ -1,36 +1,7 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
 import ttg
-from ctypes import Union
 from formula import *
 from functions import atoms
-
-def aux_truth_value(formula):
-    dicionario ={}
-    for x in atoms(formula):
-        print("\nATÔMICA: ",x)
-        interpretation = eval(input('Digite uma valoração para essa atômica, True ou False:\n'))
-        dicionario[x]= interpretation
-    print(dicionario)
-    return dicionario
-    
-def truth_value(formula):
-    dicionario = aux_truth_value(formula)
-    print(formula)
-    for atom, value in dicionario.items():
-        formula = str(formula).replace(str(atom), str(value))
-    formula = str(formula).replace('∧', ' and ').replace('∨',' or ').replace('¬',' not ')
-    print(formula)
-    print(eval(formula))
-
-def satisfiability_brute_force(formula):
-    table=truth_table(formula)
-    print(table)
-    if table.valuation() == 'Contingency':
-        print("A Fórmula é satisfatível")
-    if table.valuation() == 'Tautology':
-        print("A Fórmula é Válida")
-    if table.valuation() == 'Contradiction':
-        print("A Fórmula é insatisfatível")
 
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
@@ -44,17 +15,48 @@ def is_logical_equivalence(formula1, formula2):
     pass
     # ======== YOUR CODE HERE ========
 
-
 def is_valid(formula):
     table=truth_table(formula)
     if table.valuation() == 'Tautology':
         return True 
     else:
         return False
-    # ======== YOUR CODE HERE ========
 
 def truth_table(formula):
     strFormula = str(formula).replace('∧', ' and ').replace('∨',' or ').replace('¬',' not ')
-    table = ttg.Truths(list(atoms(formula)),[strFormula])
-    return table
+    return ttg.Truths(list(atoms(formula)),[strFormula])
 
+
+def truth_value(formula, interpretation):
+    dicionario = interpretation
+    for atom, value in dicionario.items():
+        formula = str(formula).replace(str(atom), str(value))
+    formula = str(formula).replace('∧', ' and ').replace('∨',' or ').replace('¬',' not ')
+    return eval(formula)
+
+def satisfiability_brute_force(formula):
+    interpretation = {}
+    atomicas = atoms(formula)
+    for atomica in atoms(formula):
+        interpretation[str(atomica)] = None
+    result = sat(formula, atomicas, interpretation)
+    return(result)
+
+def sat(formula, atomicas, valoracao):
+    if len(atomicas) == 0:
+        if truth_value(formula, valoracao):
+            return valoracao
+        else:
+            return False
+
+    atomica = atomicas.pop()
+    interpretacao1 = valoracao.copy()
+    interpretacao2 = valoracao.copy()
+    interpretacao1[str(atomica)] = True
+    interpretacao2[str(atomica)] = False
+
+    interpretacao1_result = sat(formula, atomicas, interpretacao1)
+    if interpretacao1_result != False:
+        return interpretacao1_result
+    else:
+        return sat(formula, atomicas, interpretacao2)
